c597a65dbdaf51c92330f7a608979f44d6f6e1a0
hoverlover <hoverlover@gmail.com>
2011-12-07 10:19:27 -0600

    Can now use :focus in specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,9 @@ require 'absentee_camper'
 RSpec.configure do |config|
   config.include CampfireBot::AbsenteeCamper::Notification
   config.include CampfireBot::AbsenteeCamper::Config
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 
   config.before do
     CampfireBot::AbsenteeCamper::Logger.stub(:instance).and_return(double('logger').as_null_object)
